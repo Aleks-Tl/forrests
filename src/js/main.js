da97496cb01e4d26ef67909204fbc1c1c84ed581
Aleksandr Tloka <aleksandr.tloka@i.ua>
2022-09-11 22:11:27 +0300

    finished slider of steps

--- src/js/main.js
+++ src/js/main.js
@@ -27,11 +27,74 @@ function transformBlockHeader() {
 if (document.querySelectorAll('.book__slider').length > 0) {
   let swiperBook = new Swiper(".book-slider", {
     mousewheel: false,
-    autoHeight: true,
+    //autoHeight: true,
     speed: 1000,
     navigation: {
       nextEl: ".swiper-btn-next",
       prevEl: ".swiper-btn-prev",
     },
   });
+}
+
+
+// Custom input type "Number"
+
+if (document.querySelectorAll('.book-step-three-origin').length > 0) {
+  function catalogItemCounter(field){
+
+    var fieldCount = function(el) {
+
+      var
+        // Мин. значение
+        min = el.data('min') || false,
+
+        // Макс. значение
+        max = el.data('max') || false,
+
+        // Кнопка уменьшения кол-ва
+        dec = el.prev('.dec'),
+
+        // Кнопка увеличения кол-ва
+        inc = el.next('.inc');
+
+      function init(el) {
+        if(!el.attr('disabled')){
+          dec.on('click', decrement);
+          inc.on('click', increment);
+        }
+
+        // Уменьшим значение
+        function decrement() {
+          var value = parseInt(el[0].value);
+          value--;
+
+          if(!min || value >= min) {
+            el[0].value = value;
+          }
+        };
+
+        // Увеличим значение
+        function increment() {
+          var value = parseInt(el[0].value);
+
+          value++;
+
+          if(!max || value <= max) {
+            el[0].value = value++;
+          }
+        };
+
+      }
+
+      el.each(function() {
+        init($(this));
+      });
+    };
+
+    $(field).each(function(){
+      fieldCount($(this));
+    });
+  }
+
+catalogItemCounter('.fieldCount');
 }
