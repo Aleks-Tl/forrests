ca25b5398fd58e8763219b6da3dad5c8ab1dc317
Aleksandr Tloka <aleksandr.tloka@i.ua>
2022-09-22 16:47:22 +0300

    fixe slider

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,7 @@ if (document.querySelectorAll('.book__slider').length > 0) {
   let swiperBook = new Swiper(".book-slider", {
     mousewheel: false,
     //autoHeight: true,
+    allowTouchMove: false,
     speed: 1000,
     navigation: {
       nextEl: ".swiper-btn-next",
